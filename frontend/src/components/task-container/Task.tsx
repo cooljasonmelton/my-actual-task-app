bd00206a354ae321120c55a0997d3929d1a693a0
Jason <jason.melton2@gmail.com>
2025-09-10 01:51:38 -0500

    add task content

--- frontend/src/components/task-container/Task.tsx
+++ frontend/src/components/task-container/Task.tsx
@@ -2,16 +2,18 @@ import { useState } from "react";
 import TaskHeader from "./TaskHeader";
 import type { TaskProps } from "./types";
 import "./Task.css";
+import TaskContent from "./TaskContent";
 
 const Task = ({ title }: TaskProps) => {
   const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <div className="card">
+    <div className="card task">
       <TaskHeader
         title={title}
         isExpanded={isExpanded}
         setIsExpanded={() => setIsExpanded(!isExpanded)}
       />
+      <TaskContent />
     </div>
   );
 };
---
+++ frontend/src/components/task-container/TaskContent.tsx
@@ -0,0 +1,7 @@
+import "./TaskContent.css";
+
+const TaskContent = () => {
+  return <div className="card">Taskcontent</div>;
+};
+
+export default TaskContent;
