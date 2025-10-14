bd24a663571788cec1aa1ae3eb83e4e08a465ad6
Jason <jason.melton2@gmail.com>
2025-10-14 17:35:52 -0500

    add confirmation modal content comp

---
+++ frontend/src/components/task-container/task/ConfirmationModalContent.tsx
@@ -0,0 +1,57 @@
+import Button from "../../design-system-components/button/Button";
+import "./ConfirmationModalContent.css";
+
+type ConfirmationModalContentProps = {
+  titleId?: string;
+  title: string;
+  confirmLabel?: string;
+  cancelLabel?: string;
+  confirmLoadingLabel?: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+  isConfirming?: boolean;
+};
+
+const ConfirmationModalContent = ({
+  titleId,
+  title,
+  confirmLabel = "yes",
+  cancelLabel = "no",
+  confirmLoadingLabel,
+  onConfirm,
+  onCancel,
+  isConfirming = false,
+}: ConfirmationModalContentProps) => {
+  const effectiveConfirmLabel =
+    isConfirming && confirmLoadingLabel ? confirmLoadingLabel : confirmLabel;
+
+  return (
+    <div className="confirmation-modal">
+      <h2 id={titleId} className="confirmation-modal__title">
+        {title}
+      </h2>
+      <div className="confirmation-modal__actions">
+        <Button
+          type="button"
+          size="small"
+          variant="primary"
+          onClick={onCancel}
+          disabled={isConfirming}
+        >
+          {cancelLabel}
+        </Button>
+        <Button
+          type="button"
+          size="small"
+          variant="secondary"
+          onClick={onConfirm}
+          disabled={isConfirming}
+        >
+          {effectiveConfirmLabel}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModalContent;
