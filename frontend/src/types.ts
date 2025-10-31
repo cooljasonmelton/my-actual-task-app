cd6327f6ade7b8ae381d0075d1bd2e35a4c56faa
Jason <jason.melton2@gmail.com>
2025-10-31 18:03:06 -0500

    update fe types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5,12 +5,14 @@ import type {
   Subtask as SharedSubtask,
   Task as SharedTask,
 } from "../../shared/types/task";
+import type { Note as SharedNote } from "../../shared/types/note";
 
 export type Status = SharedStatus;
 export type Priority = SharedPriority;
 export type Tag = SharedTag;
 export type Subtask = SharedSubtask;
 export type TaskType = SharedTask;
+export type Note = SharedNote;
 
 export type StatusSectionTabItem = {
   cta: string;
